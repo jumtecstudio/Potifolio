e91dfc65a905a00dc7787af588bbd69a8b76e224
Najib <najibjumamubiru>
2026-09-16 12:22:06 +0300

    updates to the good look

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-
+const head_text = document.getElementById("head_text")
 window.addEventListener('scroll', function (){
     if (scrollY > 30){
         const top = document.getElementById("Top")
         top.style.top = 0;
         top.style.textUnderlineOffset = "1px";
+        head_text.style.color = 'white'
+        
         
     }
     else{
